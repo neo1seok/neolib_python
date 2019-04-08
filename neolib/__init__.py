b470bdfcbf7f4f73ad20573dbdda30d56bed9c5f
neo1seok <neo1seok@gmail.com>
2019-04-08 14:37:43 +0900

    ver *1.5.20
    change json_preytty

--- neolib/__init__.py
+++ neolib/__init__.py
@@ -1,11 +1,14 @@
 
-__version__ = "1.5.19"
+__version__ = "1.5.20"
 #[ver].[majer].[miner]
 #ver: 전체 프레임의 격변이 있을때
 #majer:큰 기능 추가가 되었을때
 #miner:버그 수정및 작은 기능 추가.
 
 __history__ = """
+* 1.5.20
+change json_preytty
+	
 * 1.5.19
 change json_preytty
 	
