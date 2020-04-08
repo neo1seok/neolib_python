7a6e7d6c5efc40fd895ac1bb4ae5e1c0f2cceae1
neo1seok <neo1seok@gmail.com>
2020-04-08 14:45:37 +0900

    ver *1.6.2
    NeoAltBytes 수정.

--- neolib/__init__.py
+++ neolib/__init__.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*- 	
-__version__ = "1.6.1"
+__version__ = "1.6.2"
 """
 #[ver].[majer].[miner]
 #ver: 전체 프레임의 격변이 있을때
@@ -7,6 +7,9 @@ __version__ = "1.6.1"
 #miner:버그 수정및 작은 기능 추가.
 """
 __history__ = """
+* 1.6.2
+NeoAltBytes 수정.
+	
 * 1.6.1
 NeoAltBytes 수정.
 	
